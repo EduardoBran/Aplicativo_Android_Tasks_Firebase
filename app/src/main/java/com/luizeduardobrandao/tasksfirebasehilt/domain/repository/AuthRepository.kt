681b117977a372dcae6721735882e0db66a44cc5
EduardoBran <eduardo.ads1814@gmail.com>
2025-07-17 17:45:00 -0300

    implementando as interfaces de domain/repository para autenticacoes e operacoes CRUD

--- app/src/main/java/com/luizeduardobrandao/tasksfirebasehilt/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/luizeduardobrandao/tasksfirebasehilt/domain/repository/AuthRepository.kt
@@ -1,4 +1,22 @@
 package com.luizeduardobrandao.tasksfirebasehilt.domain.repository
 
-class AuthRepository {
+// * Define as operações de autenticação disponíveis na aplicação.
+// * Desacopla a camada de domínio das implementações concretas do Firebase.
+
+interface AuthRepository {
+
+    // Faz login com email/senha
+    suspend fun login (email: String, password: String): Result<Unit>
+
+    // Registrar um novo usuário
+    suspend fun register (email: String, password: String): Result<Unit>
+
+    // Envia email de recuperação de senha
+    suspend fun recoverAccount(email: String): Result<Unit>
+
+    // Desloga o usuário atual
+    fun logout()
+
+    // Verifica se já existe um usuário autenticado
+    fun isAuthenticated(): Boolean
 }
--- app/src/main/java/com/luizeduardobrandao/tasksfirebasehilt/domain/repository/TaskRepository.kt
+++ app/src/main/java/com/luizeduardobrandao/tasksfirebasehilt/domain/repository/TaskRepository.kt
@@ -1,4 +1,24 @@
 package com.luizeduardobrandao.tasksfirebasehilt.domain.repository
 
-class TaskRepository {
+import com.luizeduardobrandao.tasksfirebasehilt.data.model.Task
+
+// * Define as operações CRUD para o modelo Task.
+// * Permite à camada de domínio trabalhar independentemente do Firebase.
+
+interface TaskRepository {
+
+    // Retorna todas as tarefas do usuário.
+    suspend fun getTasks(): List<Task>
+
+    // Retorna uma tarefa específica pelo ID.
+    suspend fun getTaskById(id: String): Task?
+
+    // Adiciona nova tarefam retorna o ID gerado.
+    suspend fun addTask(task: Task): String
+
+    // Atualiza uma tarefa existente.
+    suspend fun updateTask(task: Task)
+
+    // Remove uma tarefa pelo ID.
+    suspend fun deleteTask(id: String)
 }
