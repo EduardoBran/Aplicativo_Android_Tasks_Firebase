dddae4de35593d1c2875288d7e4bb588166d567e
EduardoBran <eduardo.ads1814@gmail.com>
2025-07-17 16:19:40 -0300

    implementando logica para bottom sheet fragment

--- app/src/main/java/com/luizeduardobrandao/tasksfirebasehilt/ui/BottomSheetFragment.kt
+++ app/src/main/java/com/luizeduardobrandao/tasksfirebasehilt/ui/BottomSheetFragment.kt
@@ -1,17 +1,28 @@
 package com.luizeduardobrandao.tasksfirebasehilt.ui
 
+import android.app.Dialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.luizeduardobrandao.tasksfirebasehilt.R
 import com.luizeduardobrandao.tasksfirebasehilt.databinding.FragmentBottomSheetBinding
 
-class BottomSheetFragment : Fragment() {
+class BottomSheetFragment(
+    private val titleDialog: Int? = null,
+    private val titleButton: Int? = null,
+    private val message: Int,
+    private val onClick: () -> Unit = {}
+) : BottomSheetDialogFragment() {
 
     private var _binding: FragmentBottomSheetBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return BottomSheetDialog(requireContext(), theme)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +31,19 @@ class BottomSheetFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentBottomSheetBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.textSheetTitle.text =
+            getText(titleDialog ?: R.string.text_bottom_sheet_warning)
+        binding.textSheetMessage.text = getText(message)
+        binding.btnBottomSheet.text =
+            getText(titleButton ?: R.string.text_btn_bottom_sheet)
+
+        binding.btnBottomSheet.setOnClickListener {
+            onClick()
+            dismiss()
+        }
     }
 
     override fun onDestroyView() {
